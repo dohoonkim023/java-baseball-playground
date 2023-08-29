f24a61cfde47246ded569430ea7302373752e677
dohoonkim <dohoonkim023@gmail.com>
2023-08-29 23:52:52 +0900

    in progress

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,22 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split1_1() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    void split1_2() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+//    @Test
+//    void split2_2() {
+//        String actual = "(1,2)".substring(1, "(1,2)".length() -1);
+//        as
+//    }
 }
