f855e503cb47703c0e8780d090b36fb00477af24
dohoonkim <>
2023-08-30 12:53:22 +0900

    in progress

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -23,9 +24,19 @@ public class StringTest {
         assertThat(actual).containsExactly("1");
     }
 
-//    @Test
-//    void split2_2() {
-//        String actual = "(1,2)".substring(1, "(1,2)".length() -1);
-//        as
-//    }
+    @Test
+    void split2_2() {
+        String actual = "(1,2)".substring(1, "(1,2)".length() -1);
+        assertThat(actual).contains("1,2");
+    }
+
+    @Test
+    void chatAt() {
+        assertThatThrownBy(() -> {
+            "a,b,c".charAt(10);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessage("String index out of range: 10");
+//        char actual = "a,b,c".charAt(10);
+
+    }
 }
